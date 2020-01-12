d369f22f3a6ff042ce8581b28d7173b1cc5eba12
Jim Doyle <indellibleink@gmail.com>
2020-01-12 17:42:00 -0500

    Finished: Scraper works in Entity

--- ConsoleScraperEntity/EntityWriteDB.cs
+++ ConsoleScraperEntity/EntityWriteDB.cs
@@ -9,18 +9,36 @@ namespace ConsoleScraperEntity
 {
     class EntityWriteDB
     {
-        public static void EntityWrite(List<List<string>> stockTable)
+        public static void EntityWrite(List<List<string>> stockData)
         {
+            
+            using (HapDBContext db = new HapDBContext())
+            {
+                string method = "HAP";
 
-            using (HapDBContext hapStocks = new HapDBContext())
-            { 
-               for (int row=0; row<stockTable.Count; row++)
+                for (int i = 0; i < stockData.Count; i++)
                 {
-                    for (int col=0; col<stockTable.Count; col++)
+
+                    HAPStock stockTable = new HAPStock
                     {
-                        Console.WriteLine( stockTable[row][col] ); 
-                    }
+                        DateStamp = DateTime.Now,
+                        Symbol = stockData[i][0].ToString(),
+                        LastPrice = stockData[i][1].ToString(),
+                        Change = stockData[i][2].ToString(),
+                        ChgPc = stockData[i][3].ToString(),
+                        MarketTime = stockData[i][5].ToString(),
+                        Volume = stockData[i][6].ToString(),
+                        AvgVol3m = stockData[i][8].ToString(),
+                        MarketCap = stockData[i][12].ToString(),
+                        Method = method,
+
+                    };
+
+                    db.HAPStocks.Add(stockTable);
+                    db.SaveChanges();
                 }
+
+                Console.WriteLine("All data in the database:");
             }
         }
     }
